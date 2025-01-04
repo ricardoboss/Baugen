3211fef4382dba6bc06a5fa25cc645f80f0b2698
Ricardo Boss <mail@ricardoboss.de>
2025-01-04 02:29:27 +0100

    New source generator project

---
+++ Baugen/BaugenGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Baugen;
+
+[Generator]
+public class BaugenGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
